d9e0cfabfa2939770ebf714e2ff1fe229dc66051
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2021-05-01 23:01:39 +0200

    replacing class with hooks

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,52 +1,45 @@
-import React from 'react';
-import { Header } from '../Header/Header';
-import { postsService } from '../../services/postsService';
-import { Post } from './Post';
-import style from './Posts.module.css';
-import { Container } from 'react-materialize';
-import { Link } from 'react-router-dom';
-import { Loader } from '../Loader/Loader';
+import React, { useEffect, useState } from "react";
+import { Header } from "../Header/Header";
+import { postsService } from "../../services/postsService";
+import { Post } from "./Post";
+import style from "./Posts.module.css";
+import { Container } from "react-materialize";
+import { Link } from "react-router-dom";
+import { Loader } from "../Loader/Loader";
 
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-class Posts extends React.Component {
-    constructor(props) {
-        super(props);
+  useEffect(() => {
+    postsService
+      .getPosts()
+      .then((data) => {
+        setPosts(data.slice(0, 20));
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
 
-        this.state = {
-            posts: [],
-            isLoading: true
-        }
-    }
-    componentDidMount() {
-        postsService.getPosts()
-            .then(data => {
-                this.setState({ posts: data.slice(0, 20) })
-            })
-            .finally(() => this.setState({ isLoading: false }))
-    }
-
-    render() {
-        return (
-            <div className="posts">
-                <Header />
-                {this.state.isLoading
-                    ? <Loader />
-                    : <><h4 className={style.title}><i className='fa fa-file-text'></i> Posts</h4>
-                        <Container>
-                            {this.state.posts.map(post =>
-                                <Link to={`/posts/singlepost/${post.id}`} key={post.id}>
-                                    <Post
-                                        key={post.id}
-                                        id={post.id}
-                                        title={post.title}
-                                    />
-                                </Link>
-                            )}
-                        </Container></>
-                }
-
-            </div>
-        )
-    }
-}
+  return (
+    <div className="posts">
+      <Header />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <h4 className={style.title}>
+            <i className="fa fa-file-text"></i> Posts
+          </h4>
+          <Container>
+            {posts.map((post) => (
+              <Link to={`/posts/singlepost/${post.id}`} key={post.id}>
+                <Post key={post.id} id={post.id} title={post.title} />
+              </Link>
+            ))}
+          </Container>
+        </>
+      )}
+    </div>
+  );
+};
 export { Posts };
